c39221bb919c1d9d97a7e117785c4a6b88c7fc6c
Manduul <69128577+manduul0804@users.noreply.github.com>
2023-03-21 21:32:21 -0500

    init

--- C++/LEET_Container_with_most_water.cpp
+++ C++/LEET_Container_with_most_water.cpp
@@ -15,9 +15,29 @@ int main(){
 }
 
 int maxArea(vector<int>& height){
+    int y =0;
     int x = 0;
+    int count = 0;
+    int size = 0;
+    int num = 0;
+
     for(int i=0; i < height.size(); i++){
-        cout << height.at(i);
+        cout << height.at(i) << endl;
+        if(count == 0){
+            y = height.at(i);
+            x = height.at(i);
+            count++;
+            num++;
+        }
+        else if(count == 1){
+            x = height.at(i);
+            count++;
+            num++;
+        }
+        else if(((x < y) ? (x*count): (y*count)) > size){
+            cout << "size = ";
+            cout << size << endl;
+        }
     }
     return x;
 }
