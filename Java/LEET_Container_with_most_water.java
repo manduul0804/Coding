3d188c4f05e0cd3f096e39c76bc1cc803ac6f2a1
Manduul <69128577+manduul0804@users.noreply.github.com>
2023-03-21 20:12:25 -0500

    commit

---
+++ Java/LEET_Container_with_most_water.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.*;
+
+public class LEET_Container_with_most_water {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+
+      List<Integer> height = Arrays.asList(1,8,6,2,5,4,8,3,7);
+
+    }
+  }
